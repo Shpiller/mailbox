47d3a40c7d4dbc66216b5a69816d641d8e5463c4
Shpiller <reshalex@mail.ru>
2016-09-01 09:59:49 +0400

    view users

--- app/letters/letters-item/letters-item.component.js
+++ app/letters/letters-item/letters-item.component.js
@@ -1,4 +1,5 @@
 import template from './letters-item.html';
+import './letters-item.scss';
 
 export default {
     bindings: {
--- app/letters/letters-list/letters-list.component.js
+++ app/letters/letters-list/letters-list.component.js
@@ -10,7 +10,11 @@ class Controller {
         AllRestService.getAll()
             .then(all => {
                 this.mailbox = all.mailboxes.find(mailbox => mailbox._id === this.mailboxId);
-                this.letters = all.letters.filter(letter => letter.mailbox === this.mailbox._id);
+                this.letters = all.letters.filter(letter => letter.mailbox === this.mailbox._id)
+                    .map(letter => {
+                        letter.user = all.users.find(user => user.email === letter.to);
+                        return letter;
+                    })
             })
     }
 }
