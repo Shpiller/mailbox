08a8d26ef689d4dd717dcfd61bcd79bc591e204a
Shpiller <reshalex@mail.ru>
2016-09-01 10:44:45 +0400

    delete unusable

--- app/app.module.js
+++ app/app.module.js
@@ -81,29 +81,5 @@ angular.module(appSettings.moduleName, ['ui.router', 'ngCookies'])
                 $state.go('mailboxes.workflow', {}, {reload: true});
             }
         });
-
-        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, reject) {
-            console.log('$stateChangeError');
-            console.log(arguments);
-
-            if (reject && reject.status === 419) {
-                alert(reject.message);
-            }
-        });
-
-        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams, resolve) {
-            console.log('$stateChangeSuccess');
-            console.log(arguments);
-        });
-
-        $rootScope.$on('$viewContentLoaded', function (event) {
-            console.log('$viewContentLoaded');
-            console.log(arguments);
-        });
-
-        $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
-            console.log('$stateNotFound');
-            console.log(arguments);
-        });
     })
     .service('AllRestService', AllRestService);
