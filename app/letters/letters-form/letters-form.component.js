1446e2cf67e619cf2e4f7b21cbf9fdd36920dcd5
Shpiller <reshalex@mail.ru>
2016-09-01 06:25:30 +0400

    add letter

--- app/letters/letters-form/letters-form.component.js
+++ app/letters/letters-form/letters-form.component.js
@@ -6,9 +6,24 @@ import appSettings from '../../app.settings';
 class Controller {
 
     /** @ngInject */
-    constructor() {
+    constructor($scope, $state, LettersRestService) {
 
+        this._$state = $state;
+        this._LettersRestService = LettersRestService;
 
+        $scope.$watch('$ctrl.userMailboxes', (newVal, oldVal) => {
+            if(newVal){
+                const outbox = newVal.find(mailbox => mailbox.type === "Outbox");
+                this.letter = {mailbox: outbox._id};
+            }
+        });
+    }
+
+    save() {
+        this._LettersRestService.add(this.letter)
+            .then(() => {
+                this._$state.go('mailboxes.workflow.letters', {mailboxId: this.letter.mailbox});
+            })
     }
 
     cancel() {
